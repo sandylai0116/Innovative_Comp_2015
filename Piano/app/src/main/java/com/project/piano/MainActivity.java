0a2a749b14a16048db9d8e48515add30a5c9a91a
Michael Lam <o3661606@gmail.com>
2015-11-05 12:07:09 +0800

    added Pitch Processing Library

--- Piano/app/src/main/java/com/project/piano/MainActivity.java
+++ Piano/app/src/main/java/com/project/piano/MainActivity.java
@@ -1,13 +1,20 @@
 package com.project.piano;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+import be.tarsos.dsp.util.PitchConverter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult result,AudioEvent e) {
+                final float pitchInHz = result.getPitch();
+                final int midi = PitchConverter.hertzToMidiKey(new Float(pitchInHz).doubleValue());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView text = (TextView) findViewById(R.id.midiKey);
+                        text.setText("MIDI key: " + midi);
+                    }
+                    /* MIDI KEY TO MUSICAL NOTE
+                     * http://www.electronics.dit.ie/staff/tscarff/Music_technology/midi/midi_note_numbers_for_octaves.htm
+                     */
+                });
+            }
+        };
+        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.MPM, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(p);
+        Thread main = new Thread(dispatcher,"Audio Dispatcher");
+        main.start();
 
     }
 
