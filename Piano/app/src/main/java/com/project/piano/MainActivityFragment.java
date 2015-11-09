5a9c6fc6f657aac2332512b353cae25f02f8a881
sandy <sandylai0116@gmail.com>
2015-11-10 03:29:18 +0800

    generate notes

--- Piano/app/src/main/java/com/project/piano/MainActivityFragment.java
+++ Piano/app/src/main/java/com/project/piano/MainActivityFragment.java
@@ -6,14 +6,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import com.twobard.pianoview.Piano;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    PianoSheet pianoSheet;
+    Button clearNotes;
+    ToggleButton recording;
 
     public MainActivityFragment() {
     }
@@ -24,7 +34,34 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         Piano piano = (Piano) view.findViewById(R.id.piano_keyboard);
+        pianoSheet = (PianoSheet) view.findViewById(R.id.music_sheet);
+        clearNotes = (Button)view.findViewById(R.id.clear_notes);
+        recording = (ToggleButton)view.findViewById(R.id.recording);
+
+        //listener
         piano.setPianoKeyListener(onPianoKeyPress);
+        clearNotes.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Integer> noteList = new ArrayList<>();
+                pianoSheet.setNoteList(noteList);
+                pianoSheet.invalidate();
+            }
+        });
+        recording.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.i("recording button", "isChecked" + isChecked);
+            }
+        });
+
+
+        //generate notes
+        List<Integer> noteList = new ArrayList<>();
+        noteList.add(1);
+        pianoSheet.setNoteList(noteList);
+        pianoSheet.invalidate();
+
         return view;
     }
 
@@ -33,7 +70,25 @@ public class MainActivityFragment extends Fragment {
 
                 @Override
                 public void keyPressed(int id, int action) {
-                    Log.i("Keyboard", "Key pressed: " + id);
+                    if(action == 0) {
+                        Log.i("Keyboard", "Key pressed: " + id);
+                        Map<Integer,Integer> keyMap = new HashMap<>();
+                        keyMap.put(0,0);
+                        keyMap.put(2,1);
+                        keyMap.put(4,2);
+                        keyMap.put(5,3);
+                        keyMap.put(7,4);
+                        keyMap.put(9, 5);
+                        keyMap.put(11, 6);
+                        keyMap.put(12, 7);
+
+                        if(keyMap.containsKey(id)) {
+                            List<Integer> noteList = pianoSheet.getNoteList();
+                            noteList.add(keyMap.get(id));
+                            pianoSheet.setNoteList(noteList);
+                            pianoSheet.invalidate();
+                        }
+                    }
                 }
             };
 }
--- Piano/app/src/main/java/com/project/piano/PianoSheet.java
+++ Piano/app/src/main/java/com/project/piano/PianoSheet.java
@@ -4,8 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+
+import android.graphics.RectF;
+
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by LAI on 2015/11/6.
@@ -15,6 +22,15 @@ public class PianoSheet extends View {
     private final int paintColor = Color.BLACK;
     // defines paint and canvas
     private Paint paint;
+    private List<Integer> noteList = new ArrayList<>();
+
+    public List<Integer> getNoteList() {
+        return noteList;
+    }
+
+    public void setNoteList(List<Integer> noteList) {
+        this.noteList = noteList;
+    }
 
     public PianoSheet(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -36,20 +52,45 @@ public class PianoSheet extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = 5000;
-        int height = 500;
-        setMeasuredDimension(width,height);
+        int width = 20000;
+        int height = 800;
+        setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        generateNewPianoSheet(canvas, paint);
 
-        int y=canvas.getHeight()/4;
-
-        for (int line = 1; line <= 5; line++) {
-            canvas.drawLine(0, (float) y, 5000, (float) y, paint);
-            y += 30;
+        Iterator<Integer> it = noteList.iterator();
+        while (it.hasNext()) {
+            drawNatural(canvas,it.next());
         }
+    }
 
+    public void generateNewPianoSheet(Canvas canvas, Paint paint){
+        canvas.drawColor(Color.WHITE);
+        canvas.drawColor(Color.argb(17, 0, 0, 0));
+        int y = canvas.getHeight()/4;
+        for (int line = 1; line <= 5; line++) {
+            paint.setStrokeWidth(5.0f);
+            canvas.drawLine(0, (float) y, 20000, (float) y, paint);
+            y += 50;
+        }
+        canvas.translate(50, 0);
+    }
+
+    public void drawNatural(Canvas canvas, int ynote) {
+        float offset = (float) (419 - ynote * 25.7);
+        paint.setStrokeWidth(3.0f);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.translate(0, offset);
+        RectF oval1 = new RectF(0, 10, 60,60);
+        canvas.drawOval(oval1, paint);
+        paint.setStrokeWidth(7.0f);
+        if(ynote<6)canvas.drawLine(55,20,55,-135,paint);
+        else canvas.drawLine(7,25,7,190,paint);
+        if(ynote==0) canvas.drawLine(-10,30,73,30,paint);
+        canvas.translate(0, -offset);
+        canvas.translate(150, 0);
     }
 }
